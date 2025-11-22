2a5891ceeac5ce6e1823bcd593a43ae5cb416f76
Ali Hamas <alihamasdev@gmail.com>
2025-11-22 14:44:05 +0500

    chore: update to prisma@7.0.0

---
+++ prisma.config.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+	schema: "prisma/schema.prisma",
+	datasource: {
+		url: env("DATABASE_URL"),
+		shadowDatabaseUrl: env("DIRECT_URL")
+	}
+});
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,14 @@
-import { PrismaClient } from "@/lib/prisma";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+import { PrismaClient } from "@/lib/prisma/client";
+
+const adapter = new PrismaPg({
+	connectionString: process.env.DATABASE_URL
+});
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-const prisma = globalForPrisma.prisma || new PrismaClient({ log: ["query"] });
+const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
