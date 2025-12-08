b95f1f4d4e433739769583c5e98bd479ba7d3b47
Ali Hamas <alihamasdev@gmail.com>
2025-12-08 21:11:18 +0500

    feat: remove tanstack devtools

--- src/lib/tanstack/query-provider.tsx
+++ src/lib/tanstack/query-provider.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import { TanStackDevtools } from "@tanstack/react-devtools";
-import { FormDevtools } from "@tanstack/react-form-devtools";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { getQueryClient } from "./get-query-client";
 
@@ -13,12 +11,7 @@ export function QueryProvider({ children }: { children: React.ReactNode }) {
 	return (
 		<QueryClientProvider client={queryClient}>
 			{children}
-			<TanStackDevtools
-				plugins={[
-					{ name: "TanStack Form", render: <FormDevtools /> },
-					{ name: "TanStack Query", render: <ReactQueryDevtoolsPanel /> }
-				]}
-			/>
+			<ReactQueryDevtools client={queryClient} />
 		</QueryClientProvider>
 	);
 }
