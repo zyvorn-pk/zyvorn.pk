9deab521f180d257e9accebd8d6089b1f54d2d20
Ali Hamas <alihamasdev@gmail.com>
2025-12-09 14:35:25 +0500

    remove zustand and make cart-context for handling cart-sheet state on global level

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,12 +1,15 @@
+import { CartProvider } from "@/context/cart-context";
 import { StoreFooter } from "@/components/store/footer";
 import { StoreHeader } from "@/components/store/header";
 
 export default function StoreLayout({ children }: LayoutProps<"/">) {
 	return (
-		<div className="flex min-h-dvh flex-col">
-			<StoreHeader />
-			<main className="flex flex-1 flex-col py-5 md:py-10">{children}</main>
-			<StoreFooter />
-		</div>
+		<CartProvider>
+			<div className="flex min-h-dvh flex-col">
+				<StoreHeader />
+				<main className="flex flex-1 flex-col py-5 md:py-10">{children}</main>
+				<StoreFooter />
+			</div>
+		</CartProvider>
 	);
 }
